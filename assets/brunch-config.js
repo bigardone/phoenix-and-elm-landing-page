4c0de0d850e08da6cfda0b2d000b0e5191e42e0b
Ricardo Garcia Vega <bigardone@gmail.com>
2017-12-01 17:30:36 +0100

    elm setup

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -2,7 +2,7 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      joinTo: 'js/app.js',
 
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
@@ -20,43 +20,49 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: 'css/app.css',
     },
     templates: {
-      joinTo: "js/app.js"
-    }
+      joinTo: 'js/app.js',
+    },
   },
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
     // By default, we set this to "/assets/static". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(static)/
+    assets: /^(static)/,
   },
 
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
+    watched: ['static', 'css', 'js', 'vendor', 'elm'],
     // Where to compile files to
-    public: "../priv/static"
+    public: '../priv/static',
   },
 
   // Configure your plugins
   plugins: {
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/vendor/]
-    }
+      ignore: [/vendor/],
+    },
+    elmBrunch: {
+      mainModules: ['src/Main.elm'],
+      elmFolder: 'elm',
+      outputFolder: '../js/elm',
+      makeParameters: ['--warn', '--debug'],
+    },
   },
 
   modules: {
     autoRequire: {
-      "js/app.js": ["js/app"]
-    }
+      'js/app.js': ['js/app'],
+    },
   },
 
   npm: {
-    enabled: true
-  }
+    enabled: true,
+  },
 };

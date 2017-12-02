9cd69299b86ac12aee061b4e06bc9d0a6cfa6718
Ricardo Garcia Vega <bigardone@gmail.com>
2017-12-02 08:21:56 +0100

    test fix

--- test/landing_page_web/controllers/page_controller_test.exs
+++ test/landing_page_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LandingPageWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Phoenix & Elm landing page"
   end
 end

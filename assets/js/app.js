b861fed2ba7dc23860543bb16b0a4529a3c22850
Ricardo Garcia Vega <bigardone@gmail.com>
2018-01-07 19:59:31 +0100

    minor refactoring

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,14 +6,12 @@ window.onloadCallback = () => {
     const app = Elm.Main.embed(formContainer);
     let recaptcha;
 
-    app.ports.initRecaptcha.subscribe(id => {
+    app.ports.initRecaptcha.subscribe((id) => {
       window.requestAnimationFrame(() => {
         recaptcha = grecaptcha.render(id, {
           hl: 'en',
           sitekey: '6LcjgjsUAAAAAHIY0kC1fz24mSnB_uinM8k6U1K-',
-          callback: result => {
-            app.ports.setRecaptchaToken.send(result);
-          },
+          callback: app.ports.setRecaptchaToken.send,
         });
       });
     });

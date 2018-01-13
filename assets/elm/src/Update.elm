3149ea420925fbeefe46ef9eb237e53bcffb50da
Ricardo Garcia Vega <bigardone@gmail.com>
2018-01-13 12:17:29 +0100

    reset token value on error

--- assets/elm/src/Update.elm
+++ assets/elm/src/Update.elm
@@ -38,13 +38,13 @@ update msg model =
             SubscribeResponse (Err (BadStatus response)) ->
                 case Decode.decodeString validationErrorsDecoder response.body of
                     Ok validationErrors ->
-                        { model | subscribeForm = Invalid formFields validationErrors } ! [ Ports.resetRecaptcha () ]
+                        { model | subscribeForm = Invalid { formFields | recaptchaToken = Nothing } validationErrors } ! [ Ports.resetRecaptcha () ]
 
                     Err error ->
-                        { model | subscribeForm = Errored formFields "Oops! Something went wrong!" } ! [ Ports.resetRecaptcha () ]
+                        { model | subscribeForm = Errored { formFields | recaptchaToken = Nothing } "Oops! Something went wrong!" } ! [ Ports.resetRecaptcha () ]
 
             SubscribeResponse (Err error) ->
-                { model | subscribeForm = Errored formFields "Oops! Something went wrong!" } ! [ Ports.resetRecaptcha () ]
+                { model | subscribeForm = Errored { formFields | recaptchaToken = Nothing } "Oops! Something went wrong!" } ! [ Ports.resetRecaptcha () ]
 
             SetRecaptchaToken token ->
                 { model | subscribeForm = Editing { formFields | recaptchaToken = Just token } } ! []
